126bb355b9116db20f1ef670ebd9ed1861bbb3f8
Brandon Gunthner <typhus733@gmail.com>
2022-01-27 20:07:35 -0600

    Defined Model

---
+++ WebApplication1/Models/Command.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIpractice.Models
+{
+    public class Command
+    {
+        public int ID { get; set; }
+        public string HowTo { get; set; }
+        public string Line { get; set; }
+        public string Platform { get; set; }
+
+    }
+}
