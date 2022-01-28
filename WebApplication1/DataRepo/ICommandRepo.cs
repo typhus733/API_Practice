9be51ad82ec00b1964cfc23d2b244c85d9eb9ef3
Brandon Gunthner <typhus733@gmail.com>
2022-01-27 20:18:07 -0600

    Defined Command Interface

---
+++ WebApplication1/DataRepo/ICommandRepo.cs
@@ -0,0 +1,15 @@
+﻿using APIpractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIpractice.DataRepo
+{
+    public interface ICommandRepo
+    {
+        IEnumerable<Command> GetAllCommands();
+        Command GetCommandById(int id);
+
+    }
+}
