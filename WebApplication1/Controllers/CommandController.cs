69a9d374cae478228c1be3e4ea5e433b7c28a5d3
Brandon Gunthner <typhus733@gmail.com>
2022-01-27 21:07:34 -0600

    Added initial controller

---
+++ WebApplication1/Controllers/CommandController.cs
@@ -0,0 +1,33 @@
+﻿using APIpractice.DataRepo;
+using APIpractice.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIpractice.Controllers
+{
+    [Route("api/command")]
+    [ApiController]
+    public class CommandController : ControllerBase
+    {
+        private readonly MockCommandRepo _repository = new MockCommandRepo();
+        
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands()
+         {
+            var commandItems = _repository.GetAllCommands();
+
+            return Ok(commandItems);
+         }
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+         {
+            var commandItem = _repository.GetCommandById(id);
+
+            return Ok(commandItem);
+         }
+    }
+}
